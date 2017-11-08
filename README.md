05ae9d4eeb891f44ca59c14f9123edcd69b470f3
Alan Christensen <afly32@gmail.com>
2017-11-07 22:09:11 -0700

    add credentials.php

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ accent-php
 ==========
 
 The PHP version of the accent appointment book.
+
+You need a file named credentials.php in the includes folder with the contents
+~~~
+<?php
+
+  define('DB_SERVER', 'test.com');
+  define('DB_USER', 'user');
+  define('DB_PASSWORD', 'password');
+  define('DB_NAME', 'database name');
+
+?>
+~~~
--- classes/MySQL.php
+++ classes/MySQL.php
@@ -1,25 +1,6 @@
 <?php
 
-// require_once '../includes/constants.php';
-
-// define(DB_SERVER, 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/');
-define(DB_SERVER, $_ENV['OPENSHIFT_MYSQL_DB_HOST']);
-define(DB_USER, $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
-define(DB_PASSWORD, $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);
-define(DB_NAME, $_ENV['OPENSHIFT_APP_NAME']);
-
-$dbuser = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-$dbpass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-$dbname = $_ENV['OPENSHIFT_APP_NAME'];
-$dbport = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
-
-echo exec(" mysqldump  -u'$dbuser' -p'$dbpass' $dbname > db.sql");
-
-// Height for the wrap around client names
-define(MAX_HEIGHT, 3);
-
-define(OPEN_TIME, 	'08:00:00');
-define(CLOSED_TIME, '18:00:00');
+require_once 'includes/constants.php';
 
 class MySQL {
 
